102c6da423dd0daf3e00bda3d5749c8d1f0ad7df
Howard Dean Watts <howard@hdwatts.com>
2025-02-13 01:35:47 -0500

    flex wrap

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -3,7 +3,12 @@ import { getCollection } from 'astro:content'
 
 
 export async function GET(context: any) {
-	const posts = await getCollection('blog')
+	const posts = (await getCollection('blog')).filter(
+		(post) => post.data.isPublished || import.meta.env.DEV,
+	).sort(
+		(a, b) =>
+			b.data.datePublished.getTime() - a.data.datePublished.getTime(),
+	)
 	return rss({
 		title: 'Welcome | Howard Dean Watts',
 		description: "Hey there. I am a software developer based in New York, currently at Chronograph Private Equity, where I lead the development of tools to automate the extraction of unstructured data from our clients.",
